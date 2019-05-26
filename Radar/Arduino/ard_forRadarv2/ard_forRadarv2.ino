8f988065e732263c3298767939463950ad26c7a0
caglaergun <48550941+caglaergun@users.noreply.github.com>
2019-05-26 19:13:35 +0200

    Update ard_forRadarv2.ino

--- Radar/Arduino/ard_forRadarv2/ard_forRadarv2.ino
+++ Radar/Arduino/ard_forRadarv2/ard_forRadarv2.ino
@@ -1,69 +1,279 @@
-/*
-* Author: Jose Luis Villarejo Muñoz
-* Date : Octubre 2014
-*
-* Programa para uso de un sensor de ultrasonidos BAT en una simulación de un radar
-* que se visualiza en un dispositivo móvil, enviando los datos del sensor mediante bluetooth.
-*
-*/
+#include <Servo.h>. 
+#define MotorRE 5
+#define MotorLE 10
+#define MotorL1 9
+#define MotorL2 8
+#define MotorR1 7
+#define MotorR2 6
+#define s0 A1 
+#define s1 2
+#define s2 3
+#define s3 4
+#define sensorOut 11
+int K, Y, M = 0;
+// Ultrasonik Sinyal pinleri
+const int trigPin =12;
+const int echoPin = 13;
+long duration;
+int distance;
+long sure ;
+boolean x=0;
+Servo myServo; 
 
-#include <NewPing.h>
-#include <Servo.h>
-
-#define PINSERVO 10
-#define PINTIGGER 13
-#define PINECO 12
-#define MAXDISTANCIA 200
-
-//----- ANGULOLIMITE indica el desde y el hasta del rastreo. En este caso -80º..80º
-#define ANGULOLIMITE 80
-
-//----- ANGULOSALTO es el incremento que aplicamos para mover el servo
-#define ANGULOSALTO 1
-
-Servo miServo;
-
-//Aquí hemos utilizado la libreria NewPing por comodidad y rapidez
-//   http://playground.arduino.cc/Code/NewPing
-
-NewPing ultraS(PINTIGGER, PINECO, MAXDISTANCIA);
-
-int angulo = 0;
-int distancia = 0;
-int dir = 1;
-
-void setup()
-{
-  miServo.attach(PINSERVO); 
-// Inicializamos el bluetooth. Como estoy utilizando una placa ZUM-Uno de bq lo inicializo a 19200 bps
-// que es el que marca el fabricante. Si se utiliza otro módulo Bluetooth
-// a la velocidad correspondiente a dicho módulo.
-  Serial.begin(19200);
+void setup() {
+  pinMode(MotorL1, OUTPUT);
+  pinMode(MotorL2, OUTPUT);
+  pinMode(MotorLE, OUTPUT); 
+  pinMode(MotorR1, OUTPUT);
+  pinMode(MotorR2, OUTPUT);
+  pinMode(MotorRE, OUTPUT);
+  pinMode(trigPin, OUTPUT); 
+  pinMode(echoPin, INPUT); 
+  Serial.begin(9600);
+  myServo.attach(A0);// Servo motor sinyal pini
+  pinMode(s0, OUTPUT);
+  pinMode(s1, OUTPUT);
+  pinMode(s2, OUTPUT);
+  pinMode(s3, OUTPUT);
+  pinMode(sensorOut, INPUT);
+  digitalWrite(s1,LOW); 
+  digitalWrite(s0,HIGH);  
+  Serial.begin(9600);
 }
-
-void loop()
-{
-  delay(10);
+void loop() {
+  ileri();
+  delay(100);
   
-//Con la siguiente operación normalizamos el valor que enviamos al servo, ya que la variable angulo
-//alberga valores entre -ANGULOLIMITE y +ANGULOLIMITE (-80 y 80 en este caso) y al servo hemos mandarle
-//valores entre 0..180.
-  miServo.write(angulo + ANGULOLIMITE);
-  
-  distancia = ultraS.ping_cm();
- 
-  Serial.print(angulo,DEC);
-  Serial.print(",");
-  Serial.println(distancia,DEC);
- 
- // Si la variable angulo ha llegado a uno de sus limites, cambiamos de dirección
-  if (angulo >= ANGULOLIMITE || angulo <= -ANGULOLIMITE)
+  for(int i=0; i<=180; i++){ 
+myServo.write(i);
+delay(30);
+distance = calculateDistance(); 
+Serial.print(distance);
+Serial.print(",");
+Serial.print(i);
+Serial.print(":");
+  if (distance < 30 && x==0 )
+  { 
+  x=1;
+  myServo.detach();
+  if ( 0<i<=90)
   {
-     dir = -dir;
+    
+    dur();
+    delay(1000);
+    sol();
+   delay(500);
+    ileri();
+         
+  }
+  else if ( 90<i<=180)
+  {
+     
+    dur();
+    delay(1000);
+    sag(); 
+delay(500);
+    ileri();
+      
   } 
+  }
+   if (distance<6 && x==1)
+ { 
+x=0; 
+   dur();
+   delay(1000);
+   renk_olcum();
+   delay(1000);
+   geri();
+   delay(500);
+   sol();
+   delay(500);
+   ileri();
+   myServo.attach(A0);
   
-  angulo += (dir * ANGULOSALTO);
+  } 
+}
+for(int i=180; i>0; i--){  
+  myServo.write(i);
+  delay(30);
+  distance = calculateDistance();
+Serial.print(distance);
+Serial.print(",");
+Serial.print(i);
+Serial.print(":");
+if (distance < 30 && x==0 )
+  { 
+x=1;
+  myServo.detach();
+  if ( 0<i<=90)
+  {
+     
+    dur();
+    delay(1000);
+    sag();
+    delay(500);  
+    ileri();
+
+  }
+    else if( 90<i<=180)
+  {
+  
+    dur();
+    delay(1000);
+    sol(); 
+    delay(500);
+    ileri();
+
+  }
+if (distance<6 && x==1)
+ { 
+  x=0; 
+//Serial.print(distance); 
+   dur();
+   delay(1000);
+   renk_olcum();
+   delay(500);
+   geri();
+   delay(500);
+   sag();
+   delay(500);
+   ileri();
+   myServo.attach(A0);
+   
+  }  
+
+  }
+  
+  
+}
 }
 
+int calculateDistance(){ 
+   
+  digitalWrite(trigPin, LOW); 
+  delayMicroseconds(20);
+  digitalWrite(trigPin, HIGH); 
+  delayMicroseconds(20);
+  digitalWrite(trigPin, LOW);
+  duration = pulseIn(echoPin, HIGH); 
+  distance= duration*0.034/2;
+  return distance;
 
+}
+void ileri(){  
+    delay(50);
+  digitalWrite(MotorR1, HIGH); 
+  digitalWrite(MotorR2, LOW);
+  analogWrite(MotorRE, 50); 
+  digitalWrite(MotorL1, HIGH); 
+  digitalWrite(MotorL2, LOW); 
+  analogWrite(MotorLE, 50); 
+}
+void geri(){ 
+  delay(50);
+  digitalWrite(MotorR1, LOW); 
+  digitalWrite(MotorR2, HIGH); 
+  analogWrite(MotorRE, 50); 
+  digitalWrite(MotorL1, LOW); 
+  digitalWrite(MotorL2, HIGH); 
+  analogWrite(MotorLE, 50); 
+}
+void sag(){ 
+  delay(50);
+  digitalWrite(MotorR1, HIGH); 
+  digitalWrite(MotorR2, LOW); 
+  analogWrite(MotorRE, 0); 
 
+  digitalWrite(MotorL1, HIGH); 
+  digitalWrite(MotorL2, LOW); 
+  analogWrite(MotorLE, 50); 
+}
+void sol(){ 
+  delay(50);
+  digitalWrite(MotorR1, HIGH); 
+  digitalWrite(MotorR2, LOW); 
+  analogWrite(MotorRE, 50); 
+
+  digitalWrite(MotorL1, HIGH); 
+  digitalWrite(MotorL2, LOW); 
+  analogWrite(MotorLE, 0); 
+}
+void dur(){  
+  
+  digitalWrite(MotorR1, LOW); 
+  digitalWrite(MotorR2, LOW);
+  digitalWrite(MotorL1, LOW); 
+  digitalWrite(MotorL2, LOW); 
+   analogWrite(MotorRE, 0); 
+   analogWrite(MotorLE, 0); 
+}
+
+void renk_olcum(){
+  
+  digitalWrite(s2, LOW); //Kırmızıyı filtrele
+  digitalWrite(s3, LOW);
+  K = pulseIn(sensorOut, LOW); //OUT pini üzerindeki LOW süresini okur
+  Serial.print("Kırmızı= ");
+  Serial.print(K); //Kırmızı için aldığımız değeri serial monitöre yazdır
+  Serial.print("  ");
+ 
+ 
+ 
+  digitalWrite(s2, HIGH); //Yeşili filtrele
+  digitalWrite(s3, HIGH);
+  Y = pulseIn(sensorOut, LOW); //OUT pini üzerindeki LOW süresini okur
+  Serial.print("Yeşil= ");
+  Serial.print(Y); //Yeşil için aldığımız değeri serial monitöre yazdır
+  Serial.print("   ");
+  
+ 
+  digitalWrite(s2, LOW); //Maviyi filtrele
+  digitalWrite(s3, HIGH);
+  M = pulseIn(sensorOut, LOW); //OUT pini üzerindeki LOW süresini okur
+  Serial.print("Mavi= "); 
+  Serial.print(M); //Mavi için aldığımız değeri serial monitöre yazdır
+  Serial.println();
+  
+  if(K<70 && M<70 && Y<70) //Eğer tüm renklerin yoğunluğu yüksekse: Beyaz
+  {
+  Serial.print("Renk = Beyaz   ");
+    
+  }
+  else if(K<100 && Y<M && abs(K-Y)<20)
+  
+  {
+  Serial.print("Renk = Sarı   ");
+ 
+  }
+  else if(K<100 && M<Y && abs(M-K)<20)
+  {
+  Serial.print("Renk = Magenta   ");
+ 
+  }
+  else if(Y<130 && Y<M && Y<K)
+  
+  {
+  Serial.print("Renk = Yeşil   ");
+
+  }
+  else if(M<100 && M<Y && M<K)
+
+  {
+  Serial.print("Renk = Mavi   ");
+  
+  }
+  else if(K<100 && K<Y && K<M)
+  
+  {
+  Serial.print("Renk = Kırmızı   ");
+  
+  }
+  else
+  {
+  Serial.print("Renk algılanamadı   ");//Belirlediğimiz kıstaslara uymayan diğer renkler
+  
+  }
+  
+  Serial.println(); //Serial monitörde bir satır aşağı geç
+  delay(1000);//1000 milisaniye bekle
+  }
